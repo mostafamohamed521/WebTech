0853d967bb4e22da456d6be29801736ce12f073e
Desha <mostafamohamed92895@gmail.com>
2026-07-30 20:49:26 +0300

    frontend/src/routes/AppRoutes.tsx: Add /product/:slug route

---
+++ frontend/src/routes/AppRoutes.tsx
@@ -0,0 +1,27 @@
+import { Routes, Route } from "react-router-dom";
+import Navbar from "@/components/layout/Navbar";
+import HomePage from "@/pages/Home/HomePage";
+import LoginPage from "@/pages/Auth/LoginPage";
+import RegisterPage from "@/pages/Auth/RegisterPage";
+import CartPage from "@/pages/Cart/CartPage";
+import CheckoutPage from "@/pages/Checkout/CheckoutPage";
+import OrderDetailPage from "@/pages/Account/OrderDetailPage";
+import NotFoundPage from "@/pages/Errors/NotFoundPage";
+
+export default function AppRoutes() {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+        <Route path="/cart" element={<CartPage />} />
+        <Route path="/checkout" element={<CheckoutPage />} />
+        <Route path="/account/orders/:orderNumber" element={<OrderDetailPage />} />
+        {/* TODO: category, product detail, account dashboard, admin routes */}
+        <Route path="*" element={<NotFoundPage />} />
+      </Routes>
+    </>
+  );
+}
