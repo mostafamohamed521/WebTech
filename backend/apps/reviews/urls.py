27aaf8367516ce564c266620d248b3f5fcf16926
Desha <mostafamohamed92895@gmail.com>
2026-07-30 20:49:22 +0300

    backend/apps/reviews/urls.py: Register complete /reviews/ routes

---
+++ backend/apps/reviews/urls.py
@@ -0,0 +1,12 @@
+"""
+URL routes for the reviews app.
+
+Mounted under /api/v1/reviews/ from core/urls.py.
+"""
+from django.urls import path
+
+app_name = "reviews"
+
+urlpatterns = [
+    # TODO: Register reviews endpoints here.
+]
