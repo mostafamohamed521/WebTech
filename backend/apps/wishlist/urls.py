ad822c3fbe80ce3810761059f85756f4524b4ab0
Desha <mostafamohamed92895@gmail.com>
2026-07-30 20:49:24 +0300

    backend/apps/wishlist/urls.py: Register /wishlist/ routes

---
+++ backend/apps/wishlist/urls.py
@@ -0,0 +1,12 @@
+"""
+URL routes for the wishlist app.
+
+Mounted under /api/v1/wishlist/ from core/urls.py.
+"""
+from django.urls import path
+
+app_name = "wishlist"
+
+urlpatterns = [
+    # TODO: Register wishlist endpoints here.
+]
