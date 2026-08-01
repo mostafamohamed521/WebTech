563526287c1c0ba15b32f33fd468d06135af138f
Desha <mostafamohamed92895@gmail.com>
2026-08-02 02:18:01 +0300

    core/settings/production.py: Configure production environment settings

---
+++ backend/core/settings/production.py
@@ -0,0 +1,9 @@
+"""Production settings overrides."""
+from .base import *  # noqa
+
+DEBUG = False
+SECURE_SSL_REDIRECT = True
+SESSION_COOKIE_SECURE = True
+CSRF_COOKIE_SECURE = True
+SECURE_HSTS_SECONDS = 31536000
+SECURE_HSTS_INCLUDE_SUBDOMAINS = True
