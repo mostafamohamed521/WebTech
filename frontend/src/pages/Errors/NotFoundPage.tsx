4d21dba16fb5214b50abb781e0c51e78b628ed3a
Desha <mostafamohamed92895@gmail.com>
2026-08-03 00:12:13 +0300

    src/pages/Errors/NotFoundPage.tsx: Implement the 404 error page

---
+++ frontend/src/pages/Errors/NotFoundPage.tsx
@@ -0,0 +1,7 @@
+export default function NotFoundPage() {
+  return (
+    <div className="h-screen flex items-center justify-center bg-background text-white">
+      <p className="text-2xl">404 — Page not found</p>
+    </div>
+  );
+}
